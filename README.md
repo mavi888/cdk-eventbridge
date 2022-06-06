1acf2522ce804476adadc67c84a69b5aaa678859
Villalba <lmv@f84d899822fc.ant.amazon.com>
2022-06-06 15:48:58 +0300

    First commit

--- README.md
+++ README.md
@@ -1,14 +1,199 @@
-# Welcome to your CDK TypeScript project
+# AWS CDK and AWS EventBridge Advanced Demo
 
-This is a blank project for TypeScript development with CDK.
+_Infrastructure as code framework used_: AWS CDK
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+_AWS Services used_: AWS EventBridge, AWS Lambda, AWS DynamoDB, Amazon Location Services
 
-## Useful commands
+## Summary of the demo
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+In this demo you will see:
+
+- How to build an event-driven application using Amazon EventBridge and infrastructure as code
+- How to add a central logging for all your events
+- How to use PartiQL for storing data in DynamoDB
+- How to use Lambda Layers for code that is used in multiple microservices.
+- How to use API Destinations
+- How to add Dead Letter Queues to EventBridge
+
+This demo is part of a video posted in FooBar Serverless channel. You can check the videos to see the whole demo.
+
+Important: this application uses various AWS services and there are costs associated with these services after the Free Tier usage - please see the AWS Pricing page for details. You are responsible for any AWS costs incurred. No warranty is implied in this example.
+
+## Application Architecture
+
+![Basic Arquitecture](./diagrams/architecture.png)
+
+See the flow for all the events in the [events table](event-table.md).
+
+### Services
+
+- Customer - customer information such as name, address, and email
+- Order - to create an order orchestrating all other services, and describe the order status
+- Inventory - to store inventory information of items to sell
+- Payment - to make and cancel payments, it can randomly fail (see Demo below)
+- Delivery - to estimate distance and cost of a delivery, and to start, complete, or cancel a delivery
+
+Only for the event-driven scenario:
+
+- Event Store - to store all events
+
+### API Operations
+
+To trigger the create order flow, use this url:
+
+```
+GET /order/create/{customerId}/{itemId} #
+```
+
+## Get started with this project
+
+### Requirements
+
+- AWS CLI already configured with Administrator permission
+- AWS CDK CLI installed - minimum version 2.20.0 (cdk --version)
+- NodeJS 14.x installed
+
+### Deploy this demo
+
+Deploy the project to the cloud:
+
+```
+cdk synth
+cdk deploy
+```
+
+When asked about functions that may not have authorization defined, answer (y)es. The access to those functions will be open to anyone, so keep the app deployed only for the time you need this demo running.
+
+To delete the app:
+
+```
+cdk destroy
+```
+
+### Prepare the service
+
+Load the sample data in the `data` directory, use the same stack name you entered for `sam deploy`:
+
+```bash
+./load.sh <stack-name>
+```
+
+### Create your first order
+
+Create an order for `customer-1` buing `item-1` calling the Create Order API:
+
+```
+curl -i <API Gateway URL>/order/create/customer-1/item-1
+```
+
+From the output of the command, write down the `customerId` and the `orderId`, they together identify a specific order.
+
+## Useful information about the different services of the application
+
+### Inventory service
+
+#### InventoryTable content
+
+Look at the **Items** of the `InventoryTable` in the [DynamoDB console](https://console.aws.amazon.com/dynamodb). The actual name of the table will be in the form `<stack-name>-InventoryTable-<unique-ID>`. Note that the table is empty before the first order is created.
+
+If the order was created successfully, you should have an item with status `DELIVERING`.
+
+### Payments service
+
+#### Payment successful
+
+In case you find the PaymentMade event, the next events are:
+
+- ItemRemoved (from the Inventory service)
+- DeliveryStarted (from the Delivery Service)
+
+#### Payment failed
+
+To force a failed payment, you can increase the value of the `PAYMENT_FAIL_PROBABILITY` environment variable in the configuration of the `PaymentFunction` (for example, to `0.9` or `1.0`). You can change the value directly in the Lambda console or in the SAM template (and deploy).
+
+In case you find the `PaymentFailed` event, the next events are:
+
+- `ItemUnreserved` (from the Inventory service)
+
+In the `InventoryTable`, the order has status `PAYMENT_FAILED`.
+
+### Delivery service
+
+#### Completing order delivery
+
+To move forward when a delivery starts, you need to send an event to report if the delivery has been successful (`Delivered`) or not (`DeliveryCancel`).
+
+In the [EventBridge console](https://console.aws.amazon.com/events/), choose **Event buses** and then the `AppEventBus-<stack-name>` custom event bus. Then, choose **Send events**:
+
+- In **Event source**, you can put anything you want (for exmaple, `Logistics`)
+
+- In **Detail type**, you should put either `Delivered` or `DeliveryCanceled`
+
+- In **Event detail**, you need to put a JSON object identifying the order in the format (see the `deliveryEvent.json` file in the `data` directory):
+
+```json
+{
+  "customerId": "customer-1",
+  "orderId": "..."
+}
+```
+
+After you **Send** the event, new events will appear.
+
+##### Delivered order
+
+If you send the `Delivered` event, these are the new events in the logs:
+
+- `Delivered` (the event you sent form the EventBridge console)
+- `DeliveryWasDelivered` (from the Delivery service)
+- `OrderDelivered` (from the Order service)
+
+In the `InventoryTable`, the order has status `DELIVERED`.
+
+#### Delivery canceled
+
+If you send the `DeliveryCanceled` event, these are the new events in the logs:
+
+- `DeliveryCanceled` (the event you sent form the EventBridge console)
+- `DeliveryWasCanceled` (from the Delivery service)
+- `OrderCanceled` (from the Order service)
+- `ItemReturned` (from the Inventory service)
+- `PaymentCanceled` (from the Payment service)
+
+### Event Store / Event Sourcing - Central logging of events
+
+The `EventStoreFunction` is storing all events in CloudWatch Logs and in the `EventStoreTable` in DynamoDB.
+
+The `EventStoreTable` has a primary ket composed by:
+
+- `who` : `C#<customerId>` – In this way, you can quickly get all info for a customer. Events not related to a customer will use a different initial letter. For example, product updated can set this to `P#<productId>`
+- `timeWhat` : `timestamp#eventType`
+- `eventSource` : the source of the event
+- `eventDetail` : the full JSON of the event as a string
+
+#### EventStoreFunction logs
+
+Look at the logs of the `EventStoreFunction` Lambda function. The actual name of the function will be in the form `<stack-name>-EventStoreFunction-<unique-ID>`. To find the logs, in the [Lambda console](https://console.aws.amazon.com/lambda/), select the function, then the **Monitor** tab, then choose **View logs in CloudWatch**. Note that there are no logs before the first execution of a function.
+
+Close the left pad and choose **View as text**. You can see all the event published by different services processing the order. Now there is only an order. When there are more than one order in the logs, you can filter by `orderId`, for example `"2021-09-29T16:50:20.784Z"`, including the double quotes at the start and at the end.
+
+In order, the events for the order you created are:
+
+- `OrderCreated` (from the Order service)
+- `ItemReserved` (from the Inventory service)
+- `ItemDescribed` (from the Inventory service)
+- `CustomerDescribed` (from the Customer service)
+- `DeliveryEstimated` (from the Delivery service)
+- `PaymentMade` or `PaymentFailed` (from the Payment service)
+
+The Payment service fails with a probability passed to the Lambda `PaymentFunction` in an environment variable (`PAYMENT_FAIL_PROBABILITY`) that by default has value `0.2` (20% probability to fail). You can edit the variable in the Lambda console.
+
+## Related video tutorials
+
+- [Get started with AMAZON EVENTBRIDGE, build your SERVERLESS event-driven app with AWS SAM](https://youtu.be/c493KFaWteg)
+- [What to DO when an EVENT DELIVERY FAILS? - Dead Letter Queues with Amazon EventBridge](https://youtu.be/heJbgToU04c)
+- [How to DESIGN EVENT-DRIVEN applications! The best method for production applications!](https://youtu.be/LaxH8GS8l3Q)
+- [Centralize all your EVENTS of your EVENT-DRIVEN applications - Amazon EventBridge](https://youtu.be/DGijNsXoMgke)
+- [Build Event-driven applications using 3rd party APIs - Amazon EventBridge API Destinations](https://youtu.be/tyNoiravCNw)
+- [Build Event-driven applications using 3rd party APIs - Amazon EventBridge API Destinations](https://youtu.be/heJbgToU04c)
+- [Lambda Layers video](https://youtu.be/KYZ1Dop7DYg)
--- lib/cdk-eventbridge-stack.ts
+++ lib/cdk-eventbridge-stack.ts
@@ -1,16 +1,342 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Aws, CfnOutput, SecretValue, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import { EventBus, Rule, Connection, Authorization, ApiDestination, HttpMethod, RuleTargetInput} from 'aws-cdk-lib/aws-events';
+import { Table, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
+import { Function, Runtime, Code, LayerVersion } from 'aws-cdk-lib/aws-lambda';
+import { PolicyStatement, Effect} from 'aws-cdk-lib/aws-iam'
+import {RestApi, LambdaIntegration} from "aws-cdk-lib/aws-apigateway";
+import {CfnPlaceIndex, CfnRouteCalculator} from "aws-cdk-lib/aws-location";
+import {Queue} from "aws-cdk-lib/aws-sqs";
+
+import * as targets from 'aws-cdk-lib/aws-events-targets';
 
 export class CdkEventbridgeStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Create a new bus
+    const AppEventBus = new EventBus(this, 'AppEventBus', {
+      eventBusName: `AppEventBus-${Aws.STACK_NAME}`
+    });
+    
+    // Event Sourcing / Event Store
+    const EventStoreTable = new Table(this, 'EventStoreTable', {
+      partitionKey: { 
+        name: "who", type: AttributeType.STRING },
+      sortKey: {
+        name: "timeWhat", type: AttributeType.STRING
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkEventbridgeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const EventStoreFunction = new Function(this, "EventStoreFunction", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("event-store"),
+      handler: "app.lambdaHandler",
+      environment: {
+        STORE_TABLE: EventStoreTable.tableName,
+      },
+    });
+
+    const StoreEventRule = new Rule(this, "StoreEventRule", {
+      eventBus: AppEventBus,
+      eventPattern: {   
+        source: [ { prefix: ""} ] as any[] // Matches all the events in the bus
+      },
+    })
+
+    StoreEventRule.addTarget(new targets.LambdaFunction(EventStoreFunction));
+
+    EventStoreTable.grantFullAccess(EventStoreFunction);
+
+    // Helpers layers
+    const HelpersLayer = new LayerVersion(this, 'HelpersLayer', {
+      layerVersionName: 'CDK-Events-Advanced-Helpers',
+      description: 'Layer that will be shared across multiple microservices',
+      license: 'Available under the MIT license',
+      code: Code.fromAsset("helpers-layer"),
+      compatibleRuntimes: [Runtime.NODEJS_14_X]  
+    });
+  
+    const UuidLayer = new LayerVersion(this, 'UuidLayer', {
+      layerVersionName: 'CDK-Events-Advanced-Uuid',
+      description: 'Layer that contains one version of UUID library',
+      license: 'Available under the MIT license',
+      code: Code.fromAsset("uuid-layer"),
+      compatibleRuntimes: [Runtime.NODEJS_14_X]  
+    });
+
+    // EventBridge Permissions
+    const eventbridgePutPolicy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      resources: ['*'],
+      actions: ['events:PutEvents']
+    });
+
+    // Order Microservice
+    const OrderTable = new Table(this, 'OrderTable', {
+      partitionKey: { 
+        name: "customerId", type: AttributeType.STRING },
+      sortKey: {
+        name: "orderId", type: AttributeType.STRING
+      },
+    });
+
+    const OrderFunction = new Function(this, "OrderFunction", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("order"),
+      handler: "app.lambdaHandler",
+      layers: [HelpersLayer, UuidLayer],
+      environment: {
+        ORDER_TABLE: OrderTable.tableName,
+        EVENT_BUS: AppEventBus.eventBusName
+      },
+    });
+  
+    OrderTable.grantFullAccess(OrderFunction);
+    OrderFunction.addToRolePolicy(eventbridgePutPolicy)
+
+    const OrderEventRule = new Rule(this, "OrderEventRule", {
+      eventBus: AppEventBus,
+      eventPattern: {   
+        detailType: [
+          'OrderCreate',
+          'PaymentMade',
+          'PaymentFailed',
+          'PaymentCanceled',
+          'DeliveryStarted',
+          'DeliveryWasDelivered',
+          'DeliveryWasCanceled'
+        ]
+      },
+    })
+
+    OrderEventRule.addTarget(new targets.LambdaFunction(OrderFunction));
+
+    // create the API Gateway to OrderCreate
+    const OrderCreateAPI = new RestApi(this, "OrderCreateAPI");
+
+    OrderCreateAPI.root
+      .resourceForPath("/order/{action}/{customerId}/{what}")
+      .addMethod("GET", new LambdaIntegration(OrderFunction));
+  
+    // Inventory Microservice
+    const InventoryTable = new Table(this, 'InventoryTable', {
+      partitionKey: { 
+        name: "itemId", type: AttributeType.STRING },
+    });
+
+    const InventoryFunction = new Function(this, "InventoryFunction", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("inventory"),
+      handler: "app.lambdaHandler",
+      layers: [HelpersLayer],
+      environment: {
+        INVENTORY_TABLE: InventoryTable.tableName,
+        EVENT_BUS: AppEventBus.eventBusName
+      },
+    });
+
+    InventoryTable.grantFullAccess(InventoryFunction);
+    InventoryFunction.addToRolePolicy(eventbridgePutPolicy)
+
+    const InventoryEventRule = new Rule(this, "InventoryEventRule", {
+      eventBus: AppEventBus,
+      eventPattern: {   
+        detailType: [
+          'OrderCreated',
+          'OrderCanceled',
+          'ItemReserved',
+          'PaymentMade',
+          'PaymentFailed'
+        ]
+      },
+    })
+
+    InventoryEventRule.addTarget(new targets.LambdaFunction(InventoryFunction));
+  
+     // Payment Microservice
+    const PaymentTable = new Table(this, 'PaymentTable', {
+      partitionKey: { 
+        name: "paymentId", type: AttributeType.STRING },
+    });
+
+    const PaymentFunction = new Function(this, "PaymentFunction", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("payment"),
+      handler: "app.lambdaHandler",
+      layers: [HelpersLayer, UuidLayer],
+      environment: {
+        PAYMENT_TABLE: PaymentTable.tableName,
+        PAYMENT_FAIL_PROBABILITY: '0', // Between 0 and 1
+        EVENT_BUS: AppEventBus.eventBusName
+      },
+    });
+
+    PaymentTable.grantFullAccess(PaymentFunction);
+    PaymentFunction.addToRolePolicy(eventbridgePutPolicy)
+
+    const PaymentEventRule = new Rule(this, "PaymentEventRule", {
+      eventBus: AppEventBus,
+      eventPattern: {   
+        detailType: [
+          'DeliveryEstimated',
+          'ItemReturned'
+        ]
+      },
+    })
+
+    PaymentEventRule.addTarget(new targets.LambdaFunction(PaymentFunction));
+
+    // Customer Microservice
+    const CustomerTable = new Table(this, 'CustomerTable', {
+      partitionKey: { 
+        name: "customerId", type: AttributeType.STRING },
+    });
+
+    const CustomerFunction = new Function(this, "CustomerFunction", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("customer"),
+      handler: "app.lambdaHandler",
+      layers: [HelpersLayer],
+      environment: {
+        CUSTOMER_TABLE: CustomerTable.tableName,
+        EVENT_BUS: AppEventBus.eventBusName
+      },
+    });
+
+    CustomerTable.grantFullAccess(CustomerFunction);
+    CustomerFunction.addToRolePolicy(eventbridgePutPolicy)
+
+    const CustomerEventRule = new Rule(this, "CustomerEventRule", {
+      eventBus: AppEventBus,
+      eventPattern: {   
+        detailType: [
+          'ItemDescribed'
+        ]
+      },
+    })
+
+    CustomerEventRule.addTarget(new targets.LambdaFunction(CustomerFunction));
+  
+    //Delivery Microservice
+    const MyPlaceIndex = new CfnPlaceIndex(this, 'MyPlaceIndex', {
+      dataSource: 'Esri',
+      indexName: `my-place-index-${Aws.STACK_NAME}`,
+      pricingPlan: 'RequestBasedUsage',
+    });
+    
+    const MyRouteCalculator = new CfnRouteCalculator(this, 'MyRouteCalculator', {
+      calculatorName: `my-route-calculator-${Aws.STACK_NAME}`,
+      dataSource: 'Esri',
+      pricingPlan: 'RequestBasedUsage'
+    });
+
+    const DeliveryTable = new Table(this, 'DeliveryTable', {
+      partitionKey: { 
+        name: "customerId", type: AttributeType.STRING 
+      },
+      sortKey: {
+        name: "orderId", type: AttributeType.STRING
+      }
+    });
+
+    const DeliveryFunction = new Function(this, "DeliveryFunction", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("delivery"),
+      handler: "app.lambdaHandler",
+      layers: [HelpersLayer],
+      environment: {
+        PLACE_INDEX: MyPlaceIndex.indexName,
+        ROUTE_CALCULATOR: MyRouteCalculator.calculatorName,
+        DELIVERY_TABLE: DeliveryTable.tableName,
+        EVENT_BUS: AppEventBus.eventBusName
+      },
+    });
+
+    DeliveryTable.grantFullAccess(DeliveryFunction);
+    DeliveryFunction.addToRolePolicy(eventbridgePutPolicy)
+
+    // Location service Permissions
+    const SearchLocationServicePolicy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      resources: [MyPlaceIndex.attrArn],
+      actions: ['geo:SearchPlaceIndexForText']
+    });
+
+    const CalculateLocationServicePolicy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      resources: [MyRouteCalculator.attrArn],
+      actions: ['geo:CalculateRoute']
+    });
+
+    DeliveryFunction.addToRolePolicy(SearchLocationServicePolicy);
+    DeliveryFunction.addToRolePolicy(CalculateLocationServicePolicy);
+
+    const DeliveryEventRule = new Rule(this, "DeliveryEventRule", {
+      eventBus: AppEventBus,
+      eventPattern: {   
+        detailType: [
+          'CustomerDescribed',
+          'ItemRemoved',
+          'Delivered',
+          'DeliveryCanceled',
+        ]
+      },
+    })
+
+    DeliveryEventRule.addTarget(new targets.LambdaFunction(DeliveryFunction));
+
+    // API Destination for Delivery Service
+
+    const DeliveryConnection = new Connection(this, 'DeliveryConnection', {
+      authorization: Authorization.apiKey('ApiKeyName', SecretValue.secretsManager('api-key-cdk-stack')),
+      description: 'Connection with an API key',
+    });
+    
+    const DeliveryApiDestination = new ApiDestination(this, 'DeliveryApiDestination', {
+      connection: DeliveryConnection,
+      endpoint: 'https://webhook.site/926a07bb-2d15-4521-8852-bdcb7fb31906', // TODO GET FROM PARAMETERS
+      httpMethod: HttpMethod.POST,
+      rateLimitPerSecond: 10
+    });
+    
+    const DeliveryApiEventRule = new Rule(this, 'DeliveryApiEventRule', {
+      eventBus: AppEventBus,
+      eventPattern: {   
+        detailType: [
+          'DeliveryMarkedAsStarted'
+        ]
+      },
+    });
+
+    const DeliveryServiceDLQueue = new Queue(this, 'DeliveryServiceDLQueue');
+
+    DeliveryApiEventRule.addTarget(new targets.ApiDestination(DeliveryApiDestination, {
+      deadLetterQueue: DeliveryServiceDLQueue,
+      retryAttempts: 4,
+      maxEventAge: Duration.seconds(60)
+    }));
+    
+    
+    //Outputs
+    new CfnOutput(this, "EventStoreTableOutput", {
+      value: EventStoreTable.tableName
+    });
+    new CfnOutput(this, "OrderTableOutput", {
+      value: OrderTable.tableName
+    });
+    new CfnOutput(this, "InventoryTableOutput", {
+      value: InventoryTable.tableName
+    });
+    new CfnOutput(this, "PaymentTableOutput", {
+      value: PaymentTable.tableName
+    });
+    new CfnOutput(this, "CustomerTableOutput", {
+      value: CustomerTable.tableName
+    });
   }
+
+  
+  
 }
